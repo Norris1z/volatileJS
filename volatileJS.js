3959d24104da885bf3a106e1d110d5d47d940674
Joshua Baah <jabclarion@gmail.com>
2017-09-06 14:41:17 +0000

    Update volatileJS.js

--- volatileJS.js
+++ volatileJS.js
@@ -7,3 +7,21 @@ const HomeController = {
     }
 };
 
+//router object
+const router = {
+  routes: [],
+  currentUrl: {},
+  gets(url, action, parameters=[]) {
+    this.routes.push({url:this.parseUrl(url),action,parameters});
+  },
+  parseUrl(url) {
+    if(url[0]=='/')
+      {
+        url =url.slice(1,url.length);
+      }
+    if(url[url.length-1] == '/')
+      {
+        url =url.slice(0,url.length-1);
+      }
+    return url;
+  }
